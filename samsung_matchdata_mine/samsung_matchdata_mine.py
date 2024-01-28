2f51fa13f4926878bb09607353299fe6baad1ab0
kimyt990501 <kimyt990501@naver.com>
2024-01-28 13:33:36 +0900

    삼성 승률 분석: 승률 데이터 계산 후 저장

---
+++ samsung_matchdata_mine/samsung_matchdata_mine.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+import warnings
+warnings.filterwarnings('ignore')
+
+# 한글 사용하기 위해 폰트 불러옴
+plt.rc('font', family='Malgun Gothic')
+from IPython.display import set_matplotlib_formats
+set_matplotlib_formats('retina')
